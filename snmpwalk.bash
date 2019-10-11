b23f310c989a5ed49bff9f80c6c8702b09c84706
Kaloyan Tenchov <kaloyan.tenchov@nokia.com>
2019-10-11 11:44:35 -0400

    Add Nokia 7x50 OIDs

--- snmpwalk.bash
+++ snmpwalk.bash
@@ -15,17 +15,29 @@ function run {
   CMD="snmpwalk -v 2c -c $COMM_STR $1"
   echo $CMD
 
-  echo "router:$1" >> $DIR/alias
-  $CMD 1.3.6.1.2.1.31.1.1.1.18 >> $DIR/alias
+  echo "router:$1" >> $DIR/routerName
+  $CMD 1.3.6.1.4.1.9.2.1.3 >> $DIR/routerName
 
-  echo "router:$1" >> $DIR/name
-  $CMD 1.3.6.1.2.1.31.1.1.1.1 >> $DIR/name
+  echo "router:$1" >> $DIR/routerLocation
+  $CMD 1.3.6.1.2.1.1.6 >> $DIR/routerLocation
+
+  echo "router:$1" >> $DIR/sysDescr
+  $CMD 1.3.6.1.2.1.1.1.0 >> $DIR/sysDescr
 
   echo "router:$1" >> $DIR/index
   $CMD 1.3.6.1.2.1.2.2.1.1 >> $DIR/index
 
+  echo "router:$1" >> $DIR/name
+  $CMD 1.3.6.1.2.1.31.1.1.1.1 >> $DIR/name
+
+  echo "router:$1" >> $DIR/alias
+  $CMD 1.3.6.1.2.1.31.1.1.1.18 >> $DIR/alias
+
   echo "router:$1" >> $DIR/desc
   $CMD 1.3.6.1.2.1.2.2.1.2 >> $DIR/desc
+
+  echo "router:$1" >> $DIR/ifHighSpeed
+  $CMD 1.3.6.1.2.1.31.1.1.1.15 >> $DIR/ifHighSpeed
 
   echo "router:$1" >> $DIR/ifHCInOctets
   $CMD 1.3.6.1.2.1.31.1.1.1.6 >> $DIR/ifHCInOctets
@@ -33,38 +45,35 @@ function run {
   echo "router:$1" >> $DIR/ifHCOutOctets
   $CMD 1.3.6.1.2.1.31.1.1.1.10 >> $DIR/ifHCOutOctets
 
-  echo "router:$1" >> $DIR/ifHighSpeed
-  $CMD 1.3.6.1.2.1.31.1.1.1.15 >> $DIR/ifHighSpeed
-
-  echo "router:$1" >> $DIR/routerName
-  $CMD 1.3.6.1.4.1.9.2.1.3 >> $DIR/routerName
-
-  echo "router:$1" >> $DIR/routerLocation
-  $CMD 1.3.6.1.2.1.1.6 >> $DIR/routerLocation
-
   echo "router:$1" >> $DIR/dot3adAggPortAttachedAggID
   $CMD 1.2.840.10006.300.43.1.2.1.1.13 >> $DIR/dot3adAggPortAttachedAggID
   
-  echo "router:$1" >> $DIR/sysDescr
-  $CMD 1.3.6.1.2.1.1.1.0 >> $DIR/sysDescr
-
   echo "router:$1" >> $DIR/huawei_netstream_mapping
   $CMD 1.3.6.1.4.1.2011.5.25.110.1.2.1.2 >> $DIR/huawei_netstream_mapping
 
-  echo "router:$1" >> $DIR/alcatel_vRtrIfPortID
-  $CMD 1.3.6.1.4.1.6527.3.1.2.3.4.1.5 >> $DIR/alcatel_vRtrIfPortID
+  echo "router:$1" >> $DIR/alcatel_vRtrName
+  $CMD 1.3.6.1.4.1.6527.3.1.2.3.1.1.4 >> $DIR/alcatel_vRtrName
 
-  echo "router:$1" >> $DIR/alcatel_vRtrIfAlias
-  $CMD 1.3.6.1.4.1.6527.3.1.2.3.4.1.10 >> $DIR/alcatel_vRtrIfAlias
+  echo "router:$1" >> $DIR/alcatel_tmnxCflowdVRtrIfIndexContext
+  $CMD 1.3.6.1.4.1.6527.3.1.2.19.1.19 >> $DIR/alcatel_tmnxCflowdVRtrIfIndexContext
 
   echo "router:$1" >> $DIR/alcatel_vRtrIfGlobalIndex
   $CMD 1.3.6.1.4.1.6527.3.1.2.3.4.1.63 >> $DIR/alcatel_vRtrIfGlobalIndex
 
-  echo "router:$1" >> $DIR/alcatel_tmnxCflowdVRtrIfIndexContext
-  $CMD 1.3.6.1.4.1.6527.3.1.2.19.1.19 >> $DIR/alcatel_tmnxCflowdVRtrIfIndexContext
+  echo "router:$1" >> $DIR/alcatel_vRtrIfPortID
+  $CMD 1.3.6.1.4.1.6527.3.1.2.3.4.1.5 >> $DIR/alcatel_vRtrIfPortID
 
-  echo "router:$1" >> $DIR/alcatel_tmnxCflowdVRtrIfIndexContext
-  $CMD 1.3.6.1.4.1.6527.3.1.2.19.1.19 >> $DIR/alcatel_tmnxCflowdVRtrIfIndexContext
+  echo "router:$1" >> $DIR/alcatel_vRtrIfName
+  $CMD 1.3.6.1.4.1.6527.3.1.2.3.4.1.4 >> $DIR/alcatel_vRtrIfName
+
+  echo "router:$1" >> $DIR/alcatel_vRtrIfAlias
+  $CMD 1.3.6.1.4.1.6527.3.1.2.3.4.1.10 >> $DIR/alcatel_vRtrIfAlias
+
+  echo "router:$1" >> $DIR/alcatel_vRtrIfDesc
+  $CMD 1.3.6.1.4.1.6527.3.1.2.3.4.1.34 >> $DIR/alcatel_vRtrIfDesc
+
+  echo "router:$1" >> $DIR/alcatel_vRtrIfType
+  $CMD 1.3.6.1.4.1.6527.3.1.2.3.4.1.3 >> $DIR/alcatel_vRtrIfType
 }
 
 run $ROUTER_IP;
